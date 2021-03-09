1e94bbaf3167ae5e5535a5eca0ce92aefa672f54
bhavya-verma-knoldus <bhavya.verma@knoldus.com>
2021-03-09 12:48:34 +0530

    Implemented Person class and mixed it with Ordered Trait

---
+++ Program/src/main/scala/com/knoldus/Person.scala
@@ -0,0 +1,44 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.knoldus
+
+class Person(val name: String, val age: Int) extends Ordered[Person] {
+
+  override def compare(that: Person): Int = {
+    if(this.name.equals(that.name)){
+      if(this.age == that.age) 0 else if(this.age < that.age) -1 else 1
+    }
+    else{
+      if(this.name.length == that.name.length) 0 else if(this.name.length < that.name.length) -1 else 1
+    }
+  }
+
+}
+
+object Main extends App{
+  val personOne = new Person("Test", 24)
+  val personTwo = new Person("Test", 23)
+  print((personOne > personTwo) + "\n")
+
+  val person1 = new Person("TestAgain", 24)
+  val person2 = new Person("Test", 25)
+  print((person1 < person2) + "\n")
+
+  val personFirst = new Person("Test",25)
+  val personSecond = new Person("Test",25)
+  print((personFirst <= personSecond) + "\n")
+}
